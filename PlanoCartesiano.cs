430002a9659b4a80960e4ecdafee6f82f99393fe
CarlosDnl <c.ospina2001@pascualbravo.edu.co>
2018-09-23 21:36:33 -0500

    Poo: Plano Cartesiano Herramientas de programacion

---
+++ PlanoCartesiano.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanoCartesiano
+{
+    class Program
+    {
+        class PlanoCartesiano{
+            private int x, y;
+
+            public void Cargar()
+            {
+                Console.Write("Ingrese el valor de x: ");
+                x = int.Parse(Console.ReadLine());
+                Console.Write("Ingrese el valor de y: ");
+                y = int.Parse(Console.ReadLine());
+            }
+
+            public void imprimir()
+            {
+                if (x == 0 && y == 0)
+                {
+                    Console.WriteLine("Esta en el origen del plano cartesiano");
+                }
+                else if (x > 0 && y > 0)
+                {
+                    Console.WriteLine("Primer cuadrante");
+                }
+                else if(x < 0 && y > 0)
+                {
+                    Console.WriteLine("Segundo cuadrante");
+                }
+                else if (x < 0 && y < 0)
+                {
+                    Console.WriteLine("Tercer cuadrante");
+                }
+                else if (x > 0 && y < 0)
+                {
+                    Console.WriteLine("Cuarto cuadrante");
+                }
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            PlanoCartesiano pla = new PlanoCartesiano(); //Creando una instancia de la clase
+            pla.Cargar(); //Invocando el método cargar
+            pla.imprimir(); //Invocando el método imprimir
+            Console.ReadKey();
+        }
+    }
+}

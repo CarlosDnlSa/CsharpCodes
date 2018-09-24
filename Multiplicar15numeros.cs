fe2f84d0dafe43b31f0e9509d9101ca7d9dd6dac
CarlosDnl <c.ospina2001@pascualbravo.edu.co>
2018-09-24 16:34:59 -0500

    Day 2: Poo, matrices, vectores

---
+++ Multiplicar15numeros.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TablaMultiplicar
+{
+    class Program
+    {
+        class Multiplicacion
+        {
+            int contF = 1, contC = 1, i = 0, j = 0;
+            int[,] matriz = new int[15, 15];
+
+            //Rellenando la matriz
+            public void Cargar()
+            {
+                for (i = 0; i < 15; i++)
+                {
+                    matriz[0, i] = contC++;
+                    matriz[i, 0] = contF++;
+                }
+            }
+            public void Multiplicar()
+            {
+              for(i = 0; i < 15; i++)
+                {
+                    for(j = 0; j < 15; j++)
+                    {
+                        if (i != 0 && j != 0)
+                        {
+                            matriz[i, j] = matriz[0, j] * matriz[i, 0];
+                        }
+                    }
+                }
+            }
+
+            public void Imprimir()
+            {
+                for (i = 0; i < 15; i++)
+                {
+                    for(j = 0; j < 15; j++)
+                    {
+                        Console.Write(matriz[i, j] + "\t");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Multiplicacion mul = new Multiplicacion();
+            mul.Cargar();
+            mul.Multiplicar();
+            mul.Imprimir();
+            Console.ReadKey();
+        }
+    }
+}

c06f49a0e20f42afc2855b592c683a12e1705087
CarlosDnl <c.ospina2001@pascualbravo.edu.co>
2018-09-25 23:45:49 -0500

    Poo: retornando valores

---
+++ retornas.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp26
+{
+    class retornas
+    {
+        public int Sumar(int a, int c)
+        {
+            int sum;
+            sum = a + c;
+            return sum;
+        }
+           
+    }
+}
---
+++ sumar.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            retornas ret = new retornas();
+            Console.Write("Ingrese el valor de a: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese el valor de b: ");
+            b = int.Parse(Console.ReadLine());
+            ret.Sumar(a, b);
+            Console.Write("La suma es igual a: " + ret.Sumar(a,b));
+            Console.ReadKey();
+        }
+    }
+}

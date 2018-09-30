f7a377eee456fa3e6243fc9109be78f5ecc26a1c
CarlosDnl <c.ospina2001@pascualbravo.edu.co>
2018-09-29 20:50:55 -0500

    Programa que calcula perfectos,pares y primos

---
+++ Clasedelmain.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcularMenu
+{
+    class Operaciones
+    {
+        int i, j;
+
+        public int Limite()
+        {
+            int n;
+            Console.Write("Total de numeros a guardar en el conjunto: ");
+            n = int.Parse(Console.ReadLine());
+            return n;
+        }
+
+        public int[] Cargar(int n)
+        {
+            int[] vector = new int[n];
+            for (i = 0; i < vector.Length; i++)
+            {
+                Console.Write("Ingrese los " + n + " números del conjunto: ");
+                vector[i] = int.Parse(Console.ReadLine());
+            }
+            return vector;
+        }
+        public void Par(int[] vector)
+        {
+            for (i = 0; i < vector.Length; i++)
+            {
+                if (vector[i] % 2 == 0)
+                {
+                    Console.WriteLine(vector[i] + " es par");
+                }
+                else
+                {
+                    Console.WriteLine(vector[i] + " es inpar");
+                }
+            }
+        }
+        public void Primo(int[] vector)
+        {
+            int cont = 0, pos = 0, s, div = 1,j;
+            int[] conteo = new int[vector.Length];
+
+            for (i = 0; i < vector.Length; i++)
+            {
+                cont = 0;
+                for(j = 1; j < (vector[i] + 1); j++)
+                {
+                    if(vector[i] % j == 0)
+                    {
+                        cont++;
+                    }
+                }
+
+            if (cont != 2)
+            {
+                Console.WriteLine(vector[i] + " no es primo");
+            }
+            else
+            {
+                Console.WriteLine(vector[i] + " es primo");
+            }
+            }
+        }
+
+        public void Perfecto(int[] vector)
+        {
+            int cont = 0, pos = 0, s, div = 1, j, sumar = 0;
+            int[] conteo = new int[vector.Length];
+
+            for (i = 0; i < vector.Length; i++)
+            {
+                sumar = 0;
+                for (j = 1; j < (vector[i] + 1); j++)
+                {
+                    if (vector[i] % j == 0)
+                    {
+                        if (vector[i] != j)
+                            sumar = sumar + j;
+                    }
+                }
+
+                if (sumar == vector[i] && vector[i] != 0)
+                {
+                    Console.WriteLine(vector[i] + " es perfecto");
+                }
+                else if (vector[i] == 0)
+                {
+                    Console.WriteLine(vector[i] + " no estoy segura si es un número perfecto ");
+                }
+                else
+                {
+                    Console.WriteLine(vector[i] + " no es perfecto");
+                }
+            }
+        }
+        
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcularMenu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i, opcion;
+            int[] vector;
+            Operaciones oper = new Operaciones();
+            vector = oper.Cargar(oper.Limite());
+            Console.Clear();
+            Console.WriteLine("Ingrese lo que quiere comprobar del conjunto");
+            Console.WriteLine("1.- Saber si es par" + "\n" +"2.- Saber si es primo" + "\n" + "3.- Saber si es perfecto" + "\n" + "4.- Aplicar todas opciones");
+            opcion = int.Parse(Console.ReadLine());
+
+
+            switch (opcion)
+            {
+                case 1:
+                    oper.Par(vector);
+                break;
+                case 2:
+                    oper.Primo(vector);
+                break;
+                case 3:
+                    oper.Perfecto(vector);
+                break;
+                case 4:
+                    Console.WriteLine();
+                    Console.WriteLine("--Comprobando Pares");
+                    oper.Par(vector);
+                    Console.WriteLine();
+                    Console.WriteLine("--Comprobando primos");
+                    oper.Primo(vector);
+                    Console.WriteLine();
+                    Console.WriteLine("--Comprobando perfectos");
+                    oper.Perfecto(vector);
+                    break;
+                default:
+                    Console.Write("Opcion no válida");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
